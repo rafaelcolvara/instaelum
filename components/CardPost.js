d8990b6eb35459fa1b3ee08e1e0db9ac0d755773
rafael colvara <rafael.colvara@gmail.com>
2019-03-12 14:25:36 -0300

    commit

--- components/CardPost.js
+++ components/CardPost.js
@@ -1,13 +1,13 @@
 
 import { Text, View, Image, Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
-import React from 'react';
+import React, { Component } from 'react';
 import * as Animatable from 'react-native-animatable'
 
 
-export default class CardPost extends React.Component {
+export default class CardPost extends Component {
 
 
-    constructor(props){
+    constructor(props) {
         super()
         this.state = {
             foto: props.foto
@@ -16,16 +16,17 @@ export default class CardPost extends React.Component {
 
     like = () => {
         const fotoAtualizada = {
-            ...this.state.foto ,
+            ...this.state.foto,
             likeada: !this.state.foto.likeada
         }
 
-        this.setState( {
+        this.setState({
             foto: fotoAtualizada
         })
     }
+
     render() {
-        
+
         const foto = this.state.foto
 
         return (
@@ -41,11 +42,11 @@ export default class CardPost extends React.Component {
                     style={styles.CardPostImage}
                     source={{ uri: foto.urlFoto }} />
                 <View style={styles.footer}>
-                
+
                     <LikeButton onPress={this.like} LikeActive={foto.likeada}>
-                        
+
                     </LikeButton>
-                
+
                     <Text>Descrição da fotinha</Text>
                 </View>
             </View>
@@ -53,20 +54,28 @@ export default class CardPost extends React.Component {
     }
 }
 
-const LikeButton = (props) => {
+class LikeButton extends Component {
 
+    render() {
+        const props = this.props
+        return (
+            <TouchableOpacity onPress={() => {
+                this.imagemDoBotao.bounceIn(1000)
+                props.onPress()
+            }} >
+                <Animatable.Image
+                    ref={ (ref) => this.imagemDoBotao = ref } 
+                    source={
+                    props.LikeActive
+                        ? require('../assets/s2.png')
+                        : require('../assets/s2-checked.png')}
+                    style={styles.likebutton} />
 
-return (
-    <TouchableOpacity onPress={ props.onPress } >
-          <Image source={
-            props.LikeActive 
-            ?require('../assets/s2.png')
-            :require('../assets/s2-checked.png') }
-         style={styles.likebutton} />
-        
-       </TouchableOpacity>
-       )
+            </TouchableOpacity>
+        )
+    }
 }
+
 
 const larguraTotal = Dimensions.get('screen').width
 
@@ -74,8 +83,8 @@ const styles = StyleSheet.create({
     container: { marginBottom: 15 },
     header: { padding: 15, flexDirection: 'row', alignItems: 'center' },
     headerAvatar: { width: 50, height: 50, borderRadius: 100 },
-    likebutton:{ width: 30, height: 30},
-    headerTitle: { marginLeft: 10},
-    CardPostImage:{width: larguraTotal, height: larguraTotal },
+    likebutton: { width: 30, height: 30 },
+    headerTitle: { marginLeft: 10 },
+    CardPostImage: { width: larguraTotal, height: larguraTotal },
     footer: { padding: 15, alignItems: 'flex-start' }
 })
